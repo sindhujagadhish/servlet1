ed300627bc169a8d6b66f3d914c5e86e166754f9
sindhujagadhish <sindhujagadhish1994@gmail.com>
2019-06-18 17:57:01 +0530

    message

---
+++ ServletRequest.java
@@ -0,0 +1,32 @@
+package requestservlet;
+
+import java.io.*;  
+import java.io.*;  
+import javax.servlet.*;  
+import javax.servlet.http.*;   
+
+
+public class ServletRequest extends HttpServlet 
+{    
+   public void doPost(HttpServletRequest request,HttpServletResponse response) 
+       throws ServletException, IOException 
+   {        
+       response.setContentType("text/html");      
+       PrintWriter pwriter = response.getWriter();
+       
+       String name=request.getParameter("uname");      
+       String pass=request.getParameter("upass");                
+       
+       		if(name.equals("Chaitanya") && pass.equals("beginnersbook"))
+       {          
+       			RequestDispatcher dis=request.getRequestDispatcher("welcome");          
+       			dis.forward(request, response);      
+       }     
+       else
+       {      
+          pwriter.print("User name or password is incorrect!");          
+          RequestDispatcher dis=request.getRequestDispatcher("index.html");          
+          dis.include(request, response);                                
+       }      
+   }    
+}  
